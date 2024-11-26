a41aada8ca36c36a964ec6b1e7b03b1e6aab94da
Wesley Bradford Myers Jr <wesleymyersjr@gmail.com>
2024-11-26 12:44:03 -0600

    First upload with template files.

---
+++ src/main/java/net/wbmjunior/thefirstmod/TheFirstModClient.java
@@ -0,0 +1,10 @@
+package net.wbmjunior.thefirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
